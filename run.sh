cd0f79c70ce54724b2f98491ed41336ffef9ecaa
Bradley Walters <oss@walters.app>
2021-10-24 23:52:00 -0600

    hw2: run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-python3 -m hw1-2
-python3 -m hw1-3
+python3 -m unittest
+
+python3 hw2-2a.py
+python3 hw2-2b-noasync.py
+python3 hw2-4a.py
+python3 hw2-4b.py
+python3 hw2-4c.py
