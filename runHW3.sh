2383df2c6f6af69a20ae546145e59fe2e9edf87e
Bradley Walters <oss@walters.app>
2021-11-04 22:09:23 -0600

    hw3: run.sh

---
+++ runHW3.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+python3 -m unittest
+
+echo =========
+echo HW3 - Q2a
+echo =========
+python3 hw3-2a.py
+
+echo =========
+echo HW3 - Q2b
+echo =========
+python3 hw3-2b.py
+
+echo =========
+echo HW3 - Q2c
+echo =========
+python3 hw3-2c.py
